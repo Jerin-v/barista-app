5d0502dab338fc06b35412a6b0de70a73fed5ea6
Jerin Vandannoor <108553514+Jerin-v@users.noreply.github.com.>
2024-03-13 14:02:29 -0500

    created usestate and ingredients

--- src/components/BaristaForm.jsx
+++ src/components/BaristaForm.jsx
@@ -1,17 +1,32 @@
 import React, {Component, useState} from "react";
 
 const BaristaForm = () => {
+    
+    const [inputs, setInputs] = useState({
+        'temperature': '',
+        'milk': '',
+        'syrup': '',
+        'blended': ''               
+    });
+
+    const ingredients = {
+        'temperature' : ['hot', 'lukewarm', 'cold'],
+        'syrup' : ['mocha', 'vanilal', 'toffee', 'maple', 'caramel', 'other', 'none'],
+        'milk' : ['cow', 'oat', 'goat', 'almond', 'none'],
+        'blended' : ['yes', 'turbo', 'no']
+    }
 
     const onNewDrink = () => {
 
     }
 
     const onCheckAnswer = () => {
-        
+
     }
 
     return (
         <div>
+            <h2>Hi, I'd like to order a: </h2>
             <form>
 
 
