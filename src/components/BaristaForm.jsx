e2aa46e85732d8ff334e9707b7a3554d3c000a49
Jerin Vandannoor <108553514+Jerin-v@users.noreply.github.com.>
2024-03-13 13:54:00 -0500

    added functions and buttons

--- src/components/BaristaForm.jsx
+++ src/components/BaristaForm.jsx
@@ -2,9 +2,22 @@ import React, {Component, useState} from "react";
 
 const BaristaForm = () => {
 
+    const onNewDrink = () => {
+
+    }
+
+    const onCheckAnswer = () => {
+        
+    }
+
     return (
         <div>
+            <form>
 
+
+            </form>
+            <button type="submit" className="button submit" onClick={onCheckAnswer}>Check Answer</button>
+            <button type="new-drink-button" className="button newdrink" onClick={onNewDrink}>New Drink</button>
         </div>
     )
 }
