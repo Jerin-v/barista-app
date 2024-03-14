0b0affb75ccd450010d18c74023113a4deb88a18
Jerin Vandannoor <108553514+Jerin-v@users.noreply.github.com.>
2024-03-14 15:15:37 -0500

    removed extra button

--- src/components/BaristaForm.jsx
+++ src/components/BaristaForm.jsx
@@ -149,7 +149,7 @@ const BaristaForm = () => {
 
             </form>
             <button type="submit" className="button submit" onClick={onCheckAnswer}>Check Answer</button>
-            <button type="new-drink-button" className="button newdrink" onClick={onNewDrink}>New Drink</button>
+            
         </div>
     )
 }
